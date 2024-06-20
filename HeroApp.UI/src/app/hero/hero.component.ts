473f995ed3b96e58238bb25e051bcb9363b922cc
bramirez246 <bramirez246@gmail.com>
2024-06-20 14:19:20 -0700

    added ngFor for heroList

--- HeroApp.UI/src/app/hero/hero.component.ts
+++ HeroApp.UI/src/app/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroDetailsComponent } from '../hero-details/hero-details.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [HeroDetailsComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
